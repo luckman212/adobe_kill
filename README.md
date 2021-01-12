dbd2101a95ac86ffa3b7ac53463932ac1c7be2c6
luckman212 <luke.hamburg@gmail.com>
2021-01-12 13:12:43 -0500

    initial upload

---
+++ README.md
@@ -0,0 +1,37 @@
+# adobe_kill - kills Adobe crap
+
+With the default settings, this will scan for background Adobe services every hour and kill them if you have no Adobe apps running. The list of Apps is configurable by editing the `donotkillArr` array in the `adobe_kill.sh` script.
+
+## Install
+
+1. open a Terminal and execute the following commands:
+
+    ```shell
+    git clone https://github.com/luckman212/adobe_kill.git
+
+    cd adobe_kill
+
+    cp adobe_kill.sh /usr/local/bin
+
+    sudo chown root /Library/LaunchDaemons/com.adobe_kill.daemon.plist
+
+    sudo cp com.adobe_kill.daemon.plist /Library/LaunchDaemons
+
+    sudo launchctl bootstrap system /Library/LaunchDaemons/com.adobe_kill.daemon.plist
+    ```
+
+## Uninstall
+
+1. from Terminal:
+
+    ```shell
+    sudo launchctl bootout system /Library/LaunchDaemons/com.adobe_kill.daemon.plist
+
+    sudo rm /Library/LaunchDaemons/com.adobe_kill.daemon.plist
+
+    rm /usr/local/bin/adobe_kill.sh
+    ```
+
+## Caveat
+
+I haven't tested this on any systems other than my own, which were running 10.15.7 thru 11.1, and Adobe Creative Cloud 2021/2022. Please report issues.
---
+++ adobe_kill.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+if (( EUID != 0 )); then
+  echo "must be run as root"
+  exit 1
+fi
+
+rm -f /tmp/adobe_kill.daemon.stdout 2>/dev/null
+
+# if any of these apps are running, abort
+donotkillArr=(
+  'Adobe Illustrator'
+  'Adobe InDesign'
+  'Adobe Photoshop'
+  'AdobeAcrobat'
+)
+
+procArr=(
+  'Adobe Desktop Service'
+  'Core Sync'
+  'Creative Cloud Helper'
+  ACCFinderSync
+  AdobeCRDaemon
+  AdobeIPCBroker
+  AdobeResourceSynchronizer
+)
+
+launchdArr=(
+  com.adobe.acc.installer.v2
+  com.adobe.agservice
+  com.adobe.ARMDC.Communicator
+  com.adobe.ARMDC.SMJobBlessHelper
+  com.adobe.ccxprocess
+  com.adobe.GC.AGM
+  com.adobe.AdobeCreativeCloud
+)
+
+donotkill=0
+for a in "${donotkillArr[@]}"; do
+  m="^/Applications/.*\.app/Contents/MacOS/$a( [0-9]+)?\$"
+  if pgrep -lf "$m" >/dev/null; then
+    donotkill=1
+    echo "$a is running"
+  fi
+done
+(( donotkill == 0 )) || exit
+
+for proc in "${procArr[@]}"; do
+  echo "killing $proc"
+  sudo killall -QUIT "$proc" 2>/dev/null
+done
+
+for d in "${launchdArr[@]}"; do
+  echo "stopping $d"
+  launchctl stop "$d"
+  sudo launchctl stop "$d"
+done
+
+#AdobeCRDaemon
+sudo pkill -x AdobeCRDaemon
+
+#CCLibrary
+echo "killing CCLibrary"
+sudo pkill -f '^/Library/Application Support/Adobe/Creative Cloud Libraries/CCLibrary.app/'
+
+#CCXProcess
+echo "killing CCXProcess"
+sudo pkill -f '^/Applications/Utilities/Adobe Creative Cloud Experience/CCXProcess/CCXProcess.app/'
+
+exit 0
